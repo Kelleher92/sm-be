10b3e04dbaf3f4760a13da313a6fd2af5766d1a9
Alan Jue Liu <alan.jue.liu@gmail.com>
2019-02-09 13:57:21 +0000

    adding in user login code

--- server/models/user.ts
+++ server/models/user.ts
@@ -2,57 +2,59 @@ import * as mongoose from 'mongoose';
 import * as passportLocalMongoose from 'passport-local-mongoose';
 import * as bcrypt from 'bcrypt';
 
-const skillSchema = new mongoose.Schema({
-  skill: String,
-  strength: Number,
-});
-
-interface IUser extends mongoose.Document{
-  email: string;
-  password: string;
-  name: string;
+export interface IUser extends mongoose.Document {
+    email: string;
+    password: string;
+    name: string;
 }
 
+const skillSchema = new mongoose.Schema({
+    skill: String,
+    strength: Number,
+});
+
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  level: String,
-  dept: String,
-  image: String,
-  skills: [skillSchema],
-  hash: String,
-  salt: String,
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    level: String,
+    dept: String,
+    image: String,
+    skills: [skillSchema],
+    hash: String,
+    salt: String,
 });
 
 // hashing the password before saving
 userSchema.pre<IUser>('save', function (next) {
-  let user = this;
-  bcrypt.hash(user.password, 10, function (err, hash){
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
+    let user = this;
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
 });
 
 // when returning the user object remove the password
 userSchema.set('toJSON', {
-  transform: function (doc, ret) {
-    delete ret.local.password;
-    return ret;
-  }
+    transform: function (doc, ret) {
+        delete ret.local.password;
+        return ret;
+    }
 });
+
+//authenticate input against database
 
 userSchema.plugin(passportLocalMongoose);
 
--- server/services/UserService.ts
+++ server/services/UserService.ts
@@ -1,8 +1,27 @@
 import User from '../models/user'
+import { IUser } from '../models/user'
+import * as bcrypt from 'bcrypt';
 
 export const userLogin = (req, res) => {
-    // new logic
-
+    User.findOne({email: req.email})
+        .exec(function (err, user : IUser) {
+            if (err) {
+                return err
+            } else if (!user) {
+                let err: { status?: number, message: string } = new Error('User or Password is incorrect');
+                err.status = 401;
+                return err;
+            }
+            bcrypt.compare(req.password, user.password, function (err, result) {
+                if (result === true) {
+                    return user;
+                } else {
+                    let err: { status?: number, message: string } = new Error('User or Password is incorrect');
+                    err.status = 401;
+                    return err;
+                }
+            })
+        });
 }
 
 export const createUser = (req, res) => {
